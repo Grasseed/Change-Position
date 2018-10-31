1b611c36c261629e06fbe2dbe137096db79464c6
Grasseed <frank6873088@gmail.com>
2018-10-31 16:57:36 +0800

    陣列,位置順序

---
+++ ChangePosition/ChangePosition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangePosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tAry = new string[] { "紅", "黑", "白", "黃", "綠" };
+            for (int k = tAry.Length - 1; k >= 0; k--)
+            {
+                Console.Write("tAry[{0}]={1} ", k, tAry[k]);
+            }
+            Console.Read();
+        }
+    }
+}
